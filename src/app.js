414a653a147b127460d3ee78d4e08b7e60d32d30
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-05-15 19:22:07 -0500

    feat: implements winston example from the slides

--- src/app.js
+++ src/app.js
@@ -1,25 +1,12 @@
 import express from 'express'
-import createLogger from './utils.js'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { addLogger } from './logger.js'
 
 const app = express()
+app.use(addLogger)
 
-const logger = createLogger(process.env.ENVIRONMENT)
-
-app.use((req, res, next) => {
-    req.logger = logger
-    req.logger.http('info', `${req.method} at ${req.path}`)
-    next()
-})
-
-app.get('/login', (req, res) => {
-    const { username, password } = req.query
-    if (!username) {
-        // logger.error('No se envió un username')
-        return res.send('KO')
-    }
-    res.send('Ok')
+app.get('/', (req, res) => {
+    req.logger.warn('Advertencia!!')
+    res.send({ message: 'Hola Mundo!' })
 })
 
 app.listen(8080, () => console.log('Server Up'))
